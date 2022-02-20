7cc04d5178800191615dc88ab12559179b6a5810
Rafay <rafaybinzahid2002@outlook,com>
2022-02-20 17:09:07 +0500

    os

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord, asyncio, aiohttp
 from discord.ext import commands
-import random, config, db
+import random, config, db, os
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
