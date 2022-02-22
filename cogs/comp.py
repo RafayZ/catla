17275d7da7d58bf0bfc64e0671742c64c1a4f408
Rafay <rafaybinzahid2002@outlook,com>
2022-02-22 22:01:38 +0500

    .... decreased some s

--- cogs/comp.py
+++ cogs/comp.py
@@ -15,7 +15,7 @@ class Anicomp(commands.Cog, name = "Anigame Comps"):
         );'''
         await self.bot.pool.execute('''INSERT INTO comps (names, comp)
         VALUES
-          ('ranpo', '\n For Sr/Ur Max & rare 1000-2200: \n \n Arde Ikumi Ikumi (all SR) \n \n Senku Jiraiya/Loke Dio/Gasai (all SR) \n'),
+          ('ranpo', 'For Sr/Ur Max & rare 1000-2200: \n Arde Ikumi Ikumi (all SR) \n Senku Jiraiya/Loke Dio/Gasai (all SR)'),
           ('nico', 'For Sr/Ur Max & rare 1000-2200: Izumo Mukuro Iris (2 UR)');''')
         namess = await self.bot.pool.fetchrow(""" SELECT * FROM comps WHERE names = $1;""", name )
         if namess:
