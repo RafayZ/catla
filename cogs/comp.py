092e2b45df904069f987e6008077bdce51f5920f
Rafay <rafaybinzahid2002@outlook,com>
2022-02-22 22:17:23 +0500

    warning for dcomp command, will fix by morning.

--- cogs/comp.py
+++ cogs/comp.py
@@ -19,10 +19,12 @@ class Anicomp(commands.Cog, name = "Anigame Comps"):
           ('nico', 'For Sr/Ur Max & rare 1000-2200: Izumo Mukuro Iris (2 UR)');''')
         namess = await self.bot.pool.fetchrow(""" SELECT * FROM comps WHERE names = $1;""", name )
         if namess:
-            await ctx.send("Please note that high level rare comps can also be used on SR,UR raids.")
-            return await ctx.send(f'{namess["comp"]}')
+            await ctx.send("Lonely is currently fixing the dcomp command, will take a bit of time so bear with us :). ETA: 7-9 hours since I'm sleeping rn")
+            #await ctx.send("Please note that high level rare comps can also be used on SR,UR raids.")
+            #return await ctx.send(f'{namess["comp"]}')
         else:
-            await ctx.send("Please enter a valid name or DM my master if the card of your choice is not present.")
+            await ctx.send("Lonely is currently fixing the dcomp command, will take a bit of time so bear with us :). ETA: 7-9 hours since I'm sleeping rn.")
+            #await ctx.send("Please enter a valid name or DM my master if the card of your choice is not present.")
 
 def setup(bot):
     bot.add_cog(Anicomp(bot))
