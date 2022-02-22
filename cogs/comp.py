1bdb5e85cd00e380e28b7479b33fc01b428a02d5
Rafay <rafaybinzahid2002@outlook,com>
2022-02-22 14:32:27 +0500

    updated comp cog

--- cogs/comp.py
+++ cogs/comp.py
@@ -18,6 +18,10 @@ class Anicomp(commands.Cog, name = "Anigame Comps"):
         VALUES
           (Ranpo, For Sr,Ur max & rare 1000-2200: Arde, Ikumi, Ikumi (all Sr) or Senku, Jiraiya/Loke, Dio/Gasai (all Sr)),
           (Nico, For Sr,Ur max & rare 1000-2200: Izumo, Mukuro, Iris (2 UR))''')
+        for names in comps:
+            if name == names.title():
+                return await ctx.send(comp(name))
+        await ctx.send("Please enter a valid name or DM my master if the card of your choice is not present.")
 
 def setup(bot):
     bot.add_cog(Anicomp(bot))
