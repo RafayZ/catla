5ebbaa51b64e7bf6bfc125e27eb20c916c0a6690
Rafay <rafaybinzahid2002@outlook,com>
2022-02-22 21:45:40 +0500

    more updates

--- cogs/comp.py
+++ cogs/comp.py
@@ -9,7 +9,6 @@ class Anicomp(commands.Cog, name = "Anigame Comps"):
 
     @commands.command()
     async def dcomp(self, ctx, name):
-        await ctx.send("Please note that high level rare comps can also be used on SR,UR raids.")
         comp_table = '''CREATE TABLE comps(
             names TEXT,
             comp TEXT
@@ -19,7 +18,8 @@ class Anicomp(commands.Cog, name = "Anigame Comps"):
           ('ranpo', 'For Sr/Ur Max & rare 1000-2200: Arde Ikumi Ikumi (all SR) or Senku Jiraiya/Loke Dio/Gasai (all SR)'),
           ('nico', 'For Sr/Ur Max & rare 1000-2200: Izumo Mukuro Iris (2 UR)');''')
         namess = await self.bot.pool.fetchrow(""" SELECT * FROM comps WHERE names = $1;""", name )
-        if name in namess:
+        if name in names:
+            await ctx.send("Please note that high level rare comps can also be used on SR,UR raids.")
             return await ctx.send(f'{namess["comp"]}')
         else:
             await ctx.send("Please enter a valid name or DM my master if the card of your choice is not present.")
